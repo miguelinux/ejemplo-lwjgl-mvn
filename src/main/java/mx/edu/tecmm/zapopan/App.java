2b20efc1e1bd643658a424ca2dafe53617d7bdbe
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2019-10-08 17:52:30 -0500

    create project

---
+++ src/main/java/mx/edu/tecmm/zapopan/App.java
@@ -0,0 +1,13 @@
+package mx.edu.tecmm.zapopan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
